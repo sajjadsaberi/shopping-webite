b41a40388e418065779fdf22cd99c9b2fa953593
Sajjad <sajjad.saberi1996@gmail.com>
2023-11-24 21:02:26 +0330

    get the price of search

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
 const buttons = document.querySelectorAll(".filter") ;
+const priceButton = document.getElementById("search-price").querySelector("button");
+
 
 const changeClass = (filter) => {
     buttons.forEach((button) => {
@@ -42,7 +44,13 @@ const filterHandler = (event) => {
     })
 };
 
-searchInput.addEventListener("keyup", searchHandler);
+const searchPriceHandler = event => {
+    const searchPrice = event.target.parentElement.children[0].value ;
+}
+
 buttons.forEach((button) => {
     button.addEventListener("click", filterHandler)
 });
+
+searchInput.addEventListener("keyup", searchHandler);
+priceButton.addEventListener("click", searchPriceHandler);
